7e2cb83a6636989a2bbb0f7c5a66b10fc6f7547b
Andrew Burian <andrew@burian.ca>
2016-02-24 12:40:25 -0800

    Added full command string
    Full command is reconstructed from argc and a<n> entries

--- skafe-agent/enrichment.go
+++ skafe-agent/enrichment.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -15,6 +16,7 @@ func Enricher(newEvents <-chan AuditEvent, enrichedEvents chan<- AuditEvent, log
 		// enrich the event
 		GetUser(&event)
 		GetParentProcTitle(&event)
+		GetFullCmd(&event)
 
 		// dispatch the completed event
 		enrichedEvents <- event
@@ -25,26 +27,60 @@ func GetUser(ev *AuditEvent) {
 	(*ev)["username"] = "Geoff, probably"
 }
 
+func GetFullCmd(ev *AuditEvent) {
+	argcStr, ok := (*ev)["argc"]
+	if !ok {
+		return
+	}
+
+	var argc int
+	if n, err := fmt.Sscanf(argcStr, "%d", &argc); n != 1 || err != nil {
+		return
+	}
+
+	var fullCmdBuf bytes.Buffer
+
+	for i := 0; i < argc; i++ {
+		argv := fmt.Sprintf("a%d", i)
+
+		if str, ok := (*ev)[argv]; ok {
+			fullCmdBuf.WriteString(str + " ")
+		}
+	}
+
+	(*ev)["cmd"] = fullCmdBuf.String()
+}
+
 func GetParentProcTitle(ev *AuditEvent) {
 
 	// ensure this event has a ppid
 	if ppid, ok := (*ev)["ppid"]; ok {
-
-		procFile, err := os.Open("/proc/" + ppid + "/status")
-		if err != nil {
-			return
-		}
-		defer procFile.Close()
-
-		var name string
-
-		n, err := fmt.Fscanf(procFile, "Name: %63s", &name)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		if n == 1 {
+		name, err := GetProcTitle(ppid)
+		if err == nil {
 			(*ev)["pexe"] = name
 		}
 	}
+}
+
+// Get a process name from its PID
+func GetProcTitle(pid string) (string, error) {
+
+	procFile, err := os.Open("/proc/" + pid + "/status")
+	if err != nil {
+		return "", err
+	}
+	defer procFile.Close()
+
+	var name string
+
+	n, err := fmt.Fscanf(procFile, "Name: %63s", &name)
+	if err != nil {
+		return "", err
+	}
+
+	if n == 1 {
+		return name, nil
+	}
+
+	return "", fmt.Errorf("Unknown error")
 }
