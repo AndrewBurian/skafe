c882abc76283af30181ccea9f359c7131adaed72
Andrew Burian <andrew@burian.ca>
2016-03-09 16:36:48 -0800

    Added test data for audisp plugin
    Also added simple tcp link to server for testing

--- skafe-agent/link.go
+++ skafe-agent/link.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
+	"net"
 )
 
 func ServerLink(events <-chan AuditEvent, logger *log.Logger) {
+
+	var serverConn net.Conn
+	var err error
+
+Connecting:
+	for {
+		serverConn, err = net.Dial("tcp", "192.168.0.19:6969")
+		if err != nil {
+			fmt.Println("Connection failed", err)
+		} else {
+			break Connecting
+		}
+	}
+
+	encoder := gob.NewEncoder(serverConn)
 
 	for {
 
 		event := <-events
 
-		fmt.Println(event)
+		// glob onto the network
+		encoder.Encode(event)
 	}
 }
